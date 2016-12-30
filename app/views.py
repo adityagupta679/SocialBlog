a63b272e467313f7ef21775dc40e33688ed2186f
adityagupta679 <adityagupta679@gmail.com>
2016-12-30 18:05:47 +0530

    Templates Added

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
+	params={}
+	params["Name"]="Aadi"
+	params["title"]="Hello"
+	return render_template('index.html',params=params)
+	# return "Hello Developer",302
 
-	return "Hello Developer",302
+
