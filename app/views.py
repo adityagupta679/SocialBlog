b4171accc6c4aa484d66f8f07d092af876c5af1b
adityagupta679 <adityagupta679@gmail.com>
2017-01-01 17:05:28 +0530

    Receiving Feedback and Flashing a message of acknowledgement

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template,redirect,flash,url_for
 from .forms import FeedBackForm
 
 
@@ -15,8 +15,12 @@ def index():
 
 @app.route('/feedback',methods=['GET','POST'])
 def feedback():
+	form=FeedBackForm()
+
 	params={}
 	params["Name"]="Aadi"
 	params["title"]="Feedback"
-	form=FeedBackForm()
+	if form.validate_on_submit():
+		flash("Feedback received. We will get back to you at %s"%(form.email.data))
+		return redirect(url_for('index'))
 	return render_template('feedback.html',params=params,form=form)
