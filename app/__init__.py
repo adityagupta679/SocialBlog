478a9e27793bf4c876082652639e46e496d5b373
adityagupta679 <adityagupta679@gmail.com>
2017-01-08 00:36:12 +0530

    Implementing Registrations and password change. \n Updating securrity templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore
-
+from flask_security.utils  import encrypt_password
 
 
 app=Flask(__name__)
@@ -24,13 +24,13 @@ from models import User,Role
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
-
 @app.before_first_request
 def before_first_request():
 	# create database and a test user
     try:
+    	models.db.drop_all()
         models.db.create_all()
-        user_datastore.create_user(username='test_user1',email='test_user1@test.com', password='password')
+        user_datastore.create_user(username='test_user1',email='test_user1@test.com', password=encrypt_password('password'))
         db.session.commit()
         print ("Database Created")
     except Exception, e:
