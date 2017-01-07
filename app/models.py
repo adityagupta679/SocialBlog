0d1fb95188ef401c8d4f812d008154a15adc8660
adityagupta679 <adityagupta679@gmail.com>
2017-01-07 16:00:03 +0530

    Define User and Role models update requirements flask-security

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 
+from flask_security import UserMixin, RoleMixin
+
 from app import db
 
 class Feedback(db.Model):
@@ -10,5 +12,39 @@ class Feedback(db.Model):
 	timestamp = db.Column(db.DateTime)
 	def __repr__(self):
 		return '<Email: %r , Feedback: %s , Timestamp: %r >' % (self.email,self.feedback,self.timestamp)
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return self.name
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users,
+            backref=db.backref('users', lazy='dynamic'))
+
+    def __str__(self):
+        return "<username :%s , first_name :%s , last_name :%s , email :%s ,active :%s , roles :%s>"%(self.username,self.first_name,self.last_name,self.email,self.active,self.roles)
+    def __repr__(self):
+        return "<username :%s , first_name :%s , last_name :%s , email :%s , active :%s ,  roles :%s>"%(self.username,self.first_name,self.last_name,self.email,self.active,self.roles)
+
+
 
 
